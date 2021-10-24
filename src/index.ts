953075837c8ed525ff166f3f0e2422887d885fb9
henrycunh <henrycunh@gmail.com>
2021-10-23 22:16:10 -0300

    fix: added gcs driver as an export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 import { Nuanc } from "./core/index.js"
+import { NuancGoogleCloudStorageDriver } from "./data/google-cloud-storage.js"
+
 
 export default Nuanc
+export const Drivers = {
+    GoogleCloudStorage: NuancGoogleCloudStorageDriver
+}
