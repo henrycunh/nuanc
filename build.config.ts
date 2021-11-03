321baef656874c32a46554c0c2806d0660cb0abc
henrycunh <henrycunh@gmail.com>
2021-11-02 22:34:40 -0300

    ref: changed packaging to use unbuild

---
+++ build.config.ts
@@ -0,0 +1,11 @@
+export default {
+    entries: [{
+        name: 'index',
+        input: 'src/index.ts'
+    },
+    {
+        name: 'cli',
+        input: 'src/cli.ts',
+    }],
+    declaration: true,
+}
